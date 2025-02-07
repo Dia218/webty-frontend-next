f71ab79fe2bc72debf8572268e2e59e12230ccab
dia218 <dia19dev@gmail.com>
2025-02-07 18:24:18 +0900

    fix: Revert ShardCN Avartar

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
+
 import { cn } from "@/lib/utils"
 
 const Avatar = React.forwardRef<
@@ -46,4 +47,4 @@ const AvatarFallback = React.forwardRef<
 ))
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
 
-export { Avatar, AvatarImage, AvatarFallback } 
+export { Avatar, AvatarImage, AvatarFallback }
