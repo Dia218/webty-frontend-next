4b7443547f03e14374a87a212db5de8b4f0c2811
dia218 <dia19dev@gmail.com>
2025-02-08 02:13:52 +0900

    fix: Move ReviewList WebtoonThumbnail

--- src/components/common/SmallReviewList/SmallReviewList.tsx
+++ src/components/common/SmallReviewList/SmallReviewList.tsx
@@ -1,8 +1,8 @@
-import { FeedReviewResponseDto } from '@/lib/types/review/ReviewItemResponseDto';
+import { ReviewItemResponseDto } from '@/lib/types/review/ReviewItemResponseDto';
 import SmallReviewItem from '@/components/common/SmallReviewList/SmallReviewItem';
 
 export const SmallReviewList: React.FC<{
-  reviews: FeedReviewResponseDto[];
+  reviews: ReviewItemResponseDto[];
 }> = ({ reviews }) => {
   return (
     <div className="grid grid-cols-1 gap-4">
