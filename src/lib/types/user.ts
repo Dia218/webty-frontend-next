98e1ee133035938751546860c98774df001e10c5
dia218 <dia19dev@gmail.com>
2025-02-07 17:18:34 +0900

    fix: Delete TextCommentPage & Delete Wrong User.ts

--- src/lib/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface UserDataResponse {
-  userId: number;
-  nickname: string;
-  profileImageUrl?: string;
-  email: string;
-  role: 'USER' | 'ADMIN';
-  createdAt: string;
-  modifiedAt: string;
-}
