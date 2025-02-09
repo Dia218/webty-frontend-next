ad5b7229ac329a566a989cf1eac3e4b46c8cb201
segye <tprpek@gmail.com>
2025-02-09 22:29:50 +0900

    chore: 불필요한 import 제거

--- src/app/review-update/[id]/page.tsx
+++ src/app/review-update/[id]/page.tsx
@@ -2,11 +2,8 @@
 
 import NavigationBar from '@/components/common/NavigationBar/NavigationBar';
 import ReviewUpdate from '@/components/buisness/review/UpdateReview';
-import { useSearchParams } from 'next/navigation';
 
 const ReviewUpdatePage = () => {
-  const searchParams = useSearchParams();
-
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <NavigationBar />
