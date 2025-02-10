b9cdc403223eaa9112d8b3c4740cee516e41fe76
dia218 <dia19dev@gmail.com>
2025-02-10 16:14:41 +0900

    feat: Implement CommentItem

--- src/components/common/CommentList/NestedCommentItem.tsx
+++ src/components/common/CommentList/NestedCommentItem.tsx
@@ -1,1 +1,11 @@
-// 대댓글 component
+import { CommentResponseDto } from '@/lib/types/reviewComment/CommentResponseDto';
+
+interface NestedCommentItemProps {
+  comment: Omit<CommentResponseDto, 'childComments'>;
+}
+
+const NestedCommentItem = ({ comment }: NestedCommentItemProps) => {
+  return <div>구현해주세요</div>;
+};
+
+export default NestedCommentItem;
