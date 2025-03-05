f40ea355f730572d6edce7b8a4e7a91b4a9b5b9d
yuunha <h000414@naver.com>
2025-03-05 15:54:02 +0900

    Revert "[feat]웹툰 추천 api 및 웹툰추천 컴포넌트"
    This reverts commit 30bbd8f4c0868ec1f592692cfd15ce973731462b.

--- src/lib/api/userActivity/logUserActivity.ts
+++ src/lib/api/userActivity/logUserActivity.ts
@@ -17,24 +17,3 @@ export function logUserActivity(userId: number, webtoon: WebtoonSummaryDto) {
   }).catch((error) => console.error('로그 저장 실패:', error));
 }
 
-// 웹툰 검색 함수
-export const recommendWebtoons = async (userId: number): Promise<string[]> => {
-  try {
-    const response = await fetch(`http://localhost:8080/activity/webtoon/recommend/${userId}`, {  
-      method: 'GET',
-      credentials: 'include', // 인증 쿠키를 함께 전송
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error(`서버 오류: ${response.status}`);
-    }
-    const webtoonIds: string[] = await response.json(); // 응답을 JSON으로 변환
-    return webtoonIds;
-  } catch (error) {
-    console.error('API 요청 실패: 추천 웹툰 조회 실패:', error);
-    return [];
-  }
-}
