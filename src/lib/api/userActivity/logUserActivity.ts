da1d76f28d7619fb1eb5e13894189058c2a598e5
yuunha <h000414@naver.com>
2025-03-06 11:15:41 +0900

    refactor: 로그저장api : function->const,async

--- src/lib/api/userActivity/logUserActivity.ts
+++ src/lib/api/userActivity/logUserActivity.ts
@@ -1,7 +1,7 @@
 
 import { WebtoonSummaryDto } from '../../types/webtoon/WebtoonSummaryDto';
 
-export function logUserActivity(userId: number, webtoon: WebtoonSummaryDto) {
+export const logUserActivity = async(userId: number, webtoon: WebtoonSummaryDto) => {
 
     fetch('http://localhost:8080/activity/logs', {  
     method: 'POST',
